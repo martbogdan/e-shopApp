371f8529c310194af77beeffe049049f8eab56af
martbogdan <mbogdan0123@gmail.com>
2019-08-06 22:01:44 +0300

    DB

--- src/main/java/com/internshipSoftServe/eshop/model/Category.java
+++ src/main/java/com/internshipSoftServe/eshop/model/Category.java
@@ -24,7 +24,7 @@ import lombok.ToString;
 public class Category {
 
 	@Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
 	
 	@Column(unique = true, length = 45)
--- src/main/java/com/internshipSoftServe/eshop/model/Product.java
+++ src/main/java/com/internshipSoftServe/eshop/model/Product.java
@@ -17,7 +17,7 @@ import javax.validation.constraints.Size;
 public class Product {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @Column(length = 45)
